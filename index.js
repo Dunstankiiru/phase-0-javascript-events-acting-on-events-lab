39cecf6f2de6394697e32ebcd148e385eca0e310
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-02-28 14:59:16 +0300

    addsEventActing

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+
+
+function moveDodgerLeft() {
+    let dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left, 10);
+    if (left > 0) {
+        dodger.style.left = (left - 1) + 'px';
+    }
+}
+
+function moveDodgerRight() {
+    let dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left, 10);
+    if (left < 360) {
+        dodger.style.left = (left + 1) + 'px';
+    }
+}
