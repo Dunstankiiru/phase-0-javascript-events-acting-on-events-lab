94048f9ced65fce1e76db689e02339de8e7e18fe
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-03-15 00:56:24 +0300

    completes

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 // Your code here
+const dodger = document.getElementById("dodger");
 
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+});
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        const leftNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(leftNumbers, 10);
+
+        dodger.style.left = `${left - 1}px`;
+    }
+});
 
 function moveDodgerLeft() {
-    let dodger = document.getElementById('dodger');
-    let left = parseInt(dodger.style.left, 10);
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
     if (left > 0) {
-        dodger.style.left = (left - 1) + 'px';
+        dodger.style.left = `${left - 1}px`;
     }
 }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
 
 function moveDodgerRight() {
     let dodger = document.getElementById('dodger');
@@ -16,3 +36,4 @@ function moveDodgerRight() {
         dodger.style.left = (left + 1) + 'px';
     }
 }
+
